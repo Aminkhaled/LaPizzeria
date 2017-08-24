78b34803dba9f733a91395d6df5ac3743b92b257
alsun <aminabdo8@gmail.com>
2017-08-25 00:02:53 +0200

    we continue in about us page and add advance custom fields

---
+++ page-about-us.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+<?php
+if(have_posts()):
+    while (have_posts()):the_post();
+        ?>
+        <div class="hero" style="background-image: url(<?php echo get_the_post_thumbnail_url() ?>);">
+            <div class="hero-content">
+                <div class="hero-text text-center">
+                    <h2><?php echo the_title();?></h2>
+                </div>
+                <!--            hero-text -->
+            </div>
+            <!--        hero content-->
+        </div>
+        <!--     hero -->
+        <div class="main-content container">
+            <main class="content-text text-center">
+                <?php the_content(); ?>
+            </main>
+            <!--        content-text -->
+        </div>
+        <!--    main-text -->
+<div class="container box-information clear">
+    <div class="box">
+        <div class="box-content">
+            
+        </div>
+<!--        box-content -->
+    </div>
+<!--     box -->
+</div>
+<!--        box-information -->
+        <?php
+    endwhile;
+endif;
+
+?>
+<?php get_footer(); ?>
