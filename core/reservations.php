caf7aa0be96e84a291d5f3452ac07714025b6049
alsun <aminabdo8@gmail.com>
2017-09-05 22:36:49 +0200

    reservation plugin is being bulit but i finish insert option to database

--- core/reservations.php
+++ core/reservations.php
@@ -10,30 +10,36 @@
 function lapizzera_reservarion_save(){
     global  $wpdb;
     if(isset($_POST['submit'])&& $_POST['hidden'] == "1"){
-        $name = htmlspecialchars($_POST['name']);
-        $date = htmlspecialchars($_POST['date']);
-        $email = htmlspecialchars($_POST['email']);
-        $phone= htmlspecialchars($_POST['phone']);
-        $message = htmlspecialchars($_POST['message']);
+        $name = sanitize_text_field(htmlspecialchars($_POST['name']));
+        $date = sanitize_text_field(htmlspecialchars($_POST['date']));
+        $email =sanitize_email(htmlspecialchars($_POST['email'])) ;
+        $phone= sanitize_text_field(htmlspecialchars($_POST['phone'])) ;
+        $message =sanitize_text_field(htmlspecialchars($_POST['message']));
+
+        $table = $wpdb->prefix . 'reservations';
+
+        $data = array(
+            'name' => $name,
+            'date' => $date,
+            'email' => $email,
+            'phone' => $phone,
+            'message'=> $message
+        );
+
+        $format = array(
+            "%s",
+            "%s",
+            "%s",
+            "%s",
+            "%s"
+        );
+        $wpdb->insert($table,$data,$format);
+
+        $url = get_page_by_title('Thanks for  Reservation!');
+        wp_redirect(get_permalink($url));
+        exit();
     }
     
-    $table = $wpdb->prefix . 'reservations';
-    
-    $data = array(
-        'name' => $name,
-        'date' => $date,
-         'email' => $email,
-          'phone' => $phone,
-        'message'=> $message
-    );
-    
-    $format = array(
-        "%s",
-        "%s",
-        "%s",
-        "%s",
-        "%s"
-    );
-    $wpdb->insert($table,$data,$format);
+   
 }
 add_action('init','lapizzera_reservarion_save');
